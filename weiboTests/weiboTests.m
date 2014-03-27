78587864c69969cfdb81ab8b877fc783cc08fe65
laijiawei <876733756@qq.com>
2014-03-27 21:38:11 +0800

    微博项目

---
+++ weiboTests/weiboTests.m
@@ -0,0 +1,34 @@
+//
+//  weiboTests.m
+//  weiboTests
+//
+//  Created by laijiawei on 14-3-15.
+//  Copyright (c) 2014年 laijiawei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface weiboTests : XCTestCase
+
+@end
+
+@implementation weiboTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
